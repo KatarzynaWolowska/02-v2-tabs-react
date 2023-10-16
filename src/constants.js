7b0157f37e1f4815346c81299a411734f0fd5183
Katarzyna Wolowska <katarzynawolowska@mbp-katarzyna.home>
2023-10-16 13:00:30 +0200

    cleanup, add scss

---
+++ src/constants.js
@@ -0,0 +1,31 @@
+export const TABS = [
+    {
+        id: 'tab01',
+        label: 'Intro',
+        content: `<div><h1>Intro</h1><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat id porta nibh venenatis cras sed. Et netus et malesuada fames ac. Cursus sit amet dictum sit. Lacus vel facilisis volutpat est velit egestas dui. Egestas tellus rutrum tellus pellentesque eu. Vel pharetra vel turpis nunc eget lorem dolor sed viverra. Sed enim ut sem viverra aliquet. Ac odio tempor orci dapibus ultrices in iaculis nunc sed. Quis hendrerit dolor magna eget est lorem ipsum. Tellus elementum sagittis vitae et leo duis. Ultricies mi quis hendrerit dolor magna eget est. Cursus mattis molestie a iaculis at erat pellentesque. Lobortis feugiat vivamus at augue eget arcu dictum varius duis. Nunc consequat interdum varius sit amet mattis vulputate. Netus et malesuada fames ac turpis egestas sed tempus urna. Imperdiet nulla malesuada pellentesque elit eget gravida. Purus ut faucibus pulvinar elementum integer enim. Cras sed felis eget velit.</p></div>`
+    },
+    {
+        id: 'tab02',
+        label: 'HTML',
+        content: `<div><h1>HTML</h1><p>Auctor urna nunc id cursus metus aliquam eleifend. Sit amet venenatis urna cursus. Mattis ullamcorper velit sed ullamcorper. Tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed. Phasellus egestas tellus rutrum tellus pellentesque. Donec ac odio tempor orci dapibus ultrices in iaculis. Porta nibh venenatis cras sed felis eget velit aliquet sagittis. Porta lorem mollis aliquam ut porttitor leo a. Et netus et malesuada fames ac turpis egestas integer eget. Dolor morbi non arcu risus quis varius quam quisque id. Vel eros donec ac odio tempor orci dapibus ultrices. Morbi tincidunt augue interdum velit euismod in pellentesque massa placerat. Suspendisse interdum consectetur libero id. Tellus at urna condimentum mattis pellentesque id nibh tortor. Pulvinar neque laoreet suspendisse interdum consectetur. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Dui nunc mattis enim ut tellus elementum sagittis.</p></div>`
+    },
+    {
+        id: 'tab03',
+        label: 'CSS',
+        content: `<div><h1>CSS</h1><p>Purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae. Ut tortor pretium viverra suspendisse potenti nullam ac tortor. Lacus viverra vitae congue eu consequat ac felis donec. Odio morbi quis commodo odio aenean. Aliquam ultrices sagittis orci a scelerisque. Risus nec feugiat in fermentum posuere urna. Vitae semper quis lectus nulla at volutpat diam ut. Viverra orci sagittis eu volutpat odio facilisis mauris sit amet. Pulvinar elementum integer enim neque volutpat ac tincidunt vitae. Non sodales neque sodales ut etiam sit amet. Fermentum iaculis eu non diam phasellus vestibulum lorem. Vitae congue eu consequat ac felis. Turpis in eu mi bibendum neque egestas congue. Blandit massa enim nec dui nunc mattis enim ut tellus. Facilisis volutpat est velit egestas dui id. Ipsum faucibus vitae aliquet nec ullamcorper sit amet risus nullam. Venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus.
+
+		</p></div>`
+    },
+    {
+        id: 'tab04',
+        label: 'JavaScript',
+        content: `<div><h1>JavaScript</h1><p>Sed cras ornare arcu dui vivamus arcu felis. Viverra accumsan in nisl nisi scelerisque eu ultrices vitae. Aliquam id diam maecenas ultricies mi eget. Vestibulum lectus mauris ultrices eros. Aliquet bibendum enim facilisis gravida neque convallis a cras semper. Egestas fringilla phasellus faucibus scelerisque eleifend. Facilisis magna etiam tempor orci eu lobortis elementum. Magna ac placerat vestibulum lectus mauris ultrices eros in. Aliquet porttitor lacus luctus accumsan tortor posuere. Eget velit aliquet sagittis id consectetur purus. Sollicitudin nibh sit amet commodo nulla facilisi. Dolor morbi non arcu risus quis. Sed adipiscing diam donec adipiscing tristique risus nec feugiat in. At tempor commodo ullamcorper a lacus vestibulum sed arcu non. Nec ultrices dui sapien eget. Tellus orci ac auctor augue mauris augue. Suspendisse in est ante in nibh. Adipiscing elit pellentesque habitant morbi tristique senectus et netus. Augue interdum velit euismod in.
+
+		</p></div>`
+    },
+    {
+        id: 'tab05',
+        label: 'GIT',
+        content: `<div><h1>GIT</h1><p>Ipsum faucibus vitae aliquet nec ullamcorper sit amet. Praesent semper feugiat nibh sed pulvinar proin. Habitant morbi tristique senectus et netus et. Placerat in egestas erat imperdiet. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum odio. Varius sit amet mattis vulputate enim. Odio ut enim blandit volutpat maecenas volutpat blandit aliquam etiam. Ultricies mi eget mauris pharetra. Malesuada proin libero nunc consequat interdum varius sit amet mattis. In pellentesque massa placerat duis ultricies lacus. Nisl purus in mollis nunc sed. Aliquet porttitor lacus luctus accumsan tortor. Volutpat sed cras ornare arcu dui. Etiam erat velit scelerisque in dictum non consectetur a erat. Lectus nulla at volutpat diam ut venenatis tellus in metus. Luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor. In tellus integer feugiat scelerisque varius morbi enim. Id leo in vitae turpis massa sed. Quam adipiscing vitae proin sagittis nisl rhoncus.</p></div>`
+    }
+]
